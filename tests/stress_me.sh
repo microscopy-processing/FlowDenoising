846ce75190e427d846aa018feb1613c3e438c6fb
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2023-02-17 10:42:25 +0100

    .

--- tests/stress_me.sh
+++ tests/stress_me.sh
@@ -1,4 +1,4 @@
 if test ! -f TS_026.mrc; then
     wget https://ftp.ebi.ac.uk/empiar/world_availability/10988/data/DEF/tomograms/TS_026.rec TS_026.mrc
 fi
-python flowdenoising_6.py -i TS_026.mrc -o /tmp/TS_026_SDPG.mrc -v 1
+python flowdenoising.py -i TS_026.mrc -o /tmp/TS_026_SDPG.mrc -v 1
