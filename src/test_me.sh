d9409254634388df18dde5683f0bb71db4e24c88
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2023-03-18 10:39:22 +0100

    .

--- src/test_me.sh
+++ src/test_me.sh
@@ -4,4 +4,4 @@ if test ! -f $OUTPUT_FILENAME ; then
     FILEID="1lSFb4VZ5iqR3kLdPv1dDbdOe9dKPZ--_"
     wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id="$FILEID -O $OUTPUT_FILENAME && rm -rf /tmp/cookies.txt 2> /dev/null
 fi
-python ../flowdenoising.py -i big_vol.mrc -o /tmp/big_vol_SDPG.mrc -v 1
+python flowdenoising.py -i big_vol.mrc -o /tmp/big_vol_SDPG.mrc -v 1
